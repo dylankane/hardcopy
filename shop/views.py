4d291f5da077ae41be0231a5a606faf68d271e4f
Dylan <dylankane3@gmail.com>
2023-10-03 20:56:52 +0000

    Add views for main shop and product detail pages

--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
 
 def index(request):
-    """ A view to return the index page """
+    """ A view to return main page with products,
+    including sorting and search criteria"""
 
-    return render(request, 'shop/index.html')
+    products = Product.objects.all()
+
+    context = {
+        'products': products
+    }
+    return render(request, 'shop/index.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to show individual produt and its details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product
+    }
+    return render(request, 'shop/product_detail.html', context)
