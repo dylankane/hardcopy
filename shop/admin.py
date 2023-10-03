08b1f2d715ca005afb7e65fda47bd99d322e64e6
Dylan <dylankane3@gmail.com>
2023-10-03 09:14:13 +0000

    Add customisation to admin panel

--- shop/admin.py
+++ shop/admin.py
@@ -1,7 +1,43 @@
 from django.contrib import admin
 from .models import Product, Category, Genre
 
-# Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
-admin.site.register(Genre)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'artist',
+        'genre',
+        'category',
+        'price',
+        'image',
+    )
+
+    list_filter = ('category', 'genre', 'artist')
+    search_fields = (
+        'name',
+        'artist',
+        'category__friendly_name',
+        'genre__friendly_name'
+    )
+
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Genre, GenreAdmin)
