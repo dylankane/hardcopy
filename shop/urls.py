37e90af8ffdb627d920c0c8c22509d1f2782f345
Dylan <dylankane3@gmail.com>
2023-10-03 20:57:36 +0000

    Add url for product detail template

--- shop/urls.py
+++ shop/urls.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='shop')
+    path('', views.index, name='shop'),
+    path('<product_id>', views.product_detail, name='product_detail')
 ]
