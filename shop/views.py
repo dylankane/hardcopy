053671254b8adb285fcb20c666989b9b36626626
Dylan <dylankane3@gmail.com>
2023-10-04 11:36:59 +0000

    Add more product sorting and filtering features

--- shop/views.py
+++ shop/views.py
@@ -9,10 +9,30 @@ def index(request):
     including sorting and search criteria"""
 
     products = Product.objects.all()
+    artists = Product.objects.order_by('artist').values_list('artist', flat=True).distinct()
     query = None
     categories = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+
+            if sortkey == 'release_date':
+                products = products.exclude(release_date__isnull=True)
+
+            products = products.order_by(sortkey)
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -35,10 +55,14 @@ def index(request):
                 )
             products = products.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
+        'artists': artists,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
     return render(request, 'shop/index.html', context)
 
