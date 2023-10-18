b36dc0647e22485558b1105e86a702f86c479a67
Dylan <dylankane3@gmail.com>
2023-10-18 12:47:01 +0000

    Build contact model to manage contact messages

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+
+class Contact(models.Model):
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='user_message')
+    name = models.CharField(max_length=150)
+    email = models.EmailField()
+    phone = models.CharField(
+        max_length=20, null=True, blank=True)
+    subject = models.CharField(max_length=250, null=True, blank=True)
+    message = models.TextField()
+    date_sent = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-date_sent']
+
+    def __str__(self):
+        return f"{subject} by {name}"
