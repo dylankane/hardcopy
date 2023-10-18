d3d4597fbae672d22901536425750f654efd34a5
Dylan <dylankane3@gmail.com>
2023-10-18 12:48:41 +0000

    Add class to admin.py for contact messages

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('author', 'name', 'email', 'phone', 'message', 'date_sent')
+    list_filter = ('author', 'email', 'date_sent')
+    search_fields = ('author__username', 'name', 'date_sent')
+
+
+admin.site.register(Contact, ContactAdmin)
