c1f5457b7819f64a577001b5e3d9d9fb47ab69be
Dylan <dylankane3@gmail.com>
2023-10-23 01:08:49 +0000

    Add function to use friendly name in product form

--- shop/forms.py
+++ shop/forms.py
@@ -9,15 +9,20 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = '__all__'
 
-    image = forms.ImageField(label='image', required=False, widget=CustomClearableFileInput)
+    image = forms.ImageField(
+        label='image', required=False, widget=CustomClearableFileInput)
 
-        # def __init__(self, *args, **kwargs):
-        #     super().__init__(*args, **kwargs)
-        #     categories = Category.objects.all()
-        #     friendly_names = [(
-        #         c.id, c.get_friendly_name()) for c in categories]
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+        genres = Genre.objects.all()
+        genre_friendly_names = [(g.id, g.get_friendly_name()) for g in genres]
 
-        #     self.fields['category'].choices = friendly_names
-        #     # for styling
-        #     for field_name, field in self.fields.items():
-        #         field.widget.attrs['class'] = 'border-black rounded-0'
+        self.fields['category'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
+
+        self.fields['genre'].choices = genre_friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
