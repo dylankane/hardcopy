f0a285187fa31b1538d56c763b646a152aaec970
Dylan <dylankane3@gmail.com>
2023-10-23 16:31:11 +0000

    Create and set up newsletter app

---
+++ newsletter/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Newsletter
+
+
+class NewsletterAdmin(admin.ModelAdmin):
+    list_display = ('email',)
+    list_filter = ('email', 'subscribed_on',)
+    search_fields = ('email', 'subscribed_on',)
+
+
+admin.site.register(Newsletter, NewsletterAdmin)
