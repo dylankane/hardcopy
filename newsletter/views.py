9aedaa93c55867a150af4d735ebb205b9926ad36
Dylan <dylankane3@gmail.com>
2023-10-24 02:13:21 +0000

    Refine logic in the newsletter view

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 
 from django.contrib import messages
 from .models import Newsletter
@@ -6,34 +6,22 @@ from .forms import NewsletterForm
 
 
 def newsletter(request):
-
     if request.method == 'POST':
         newsletter_form = NewsletterForm(data=request.POST)
+
         if newsletter_form.is_valid():
             newsletter_form.save()
-
             messages.success(
                 request,
-                'You have successfully subscribed to our monthly newsletter.\
-                    The newsletter will be in your inbox within \
-                    the first week of every month, enjoy!!'
-                )
-            return render(request, 'shop/index.html')
+                'You have successfully subscribed to our monthly\
+                    newsletter.The newsletter will be in your inbox within\
+                    the first week of every month, enjoy!!')
         else:
             messages.error(
                 request,
-                'An error has occurred, you have not been subscribed,\
-                    check if the email address in the form is valid')
-
-    if request.method == 'GET':
+                'An error has occurred, you have not been subscribed, check if\
+                    the email address in the form is valid')
+        return redirect(reverse('shop'))
+    else:
         newsletter_form = NewsletterForm()
-
-    # else:
-    #     newsletter_form = NewsletterForm()
-    return render(request, 'shop/index.html', {'newsletter_form': newsletter_form})
-
-    # template = 'includes/footer.html'
-    # context = {
-    #     'newsletter_form': newsletter_form,
-    # }
-    # return render(request, template, context)
+        return redirect(reverse('shop'))
