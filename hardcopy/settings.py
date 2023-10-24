c149f43c4f89bb72cd0a4730c1ae6e07d8e9e4b9
Dylan <dylankane3@gmail.com>
2023-10-24 02:16:23 +0000

    Define the context processor in the settings

--- hardcopy/settings.py
+++ hardcopy/settings.py
@@ -84,6 +84,7 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
                 'cart.contexts.cart_contents',
+                'hardcopy.context_processor.newsletter_form_processor'
             ],
             'builtins': [
                 'crispy_forms.templatetags.crispy_forms_tags',
