fe8b68e886c30f6b4b73ffbbfdaf9f603152c64b
Dylan <dylankane3@gmail.com>
2023-10-24 20:52:26 +0000

    Configure settings for deployment, add a procfile

--- hardcopy/settings.py
+++ hardcopy/settings.py
@@ -29,7 +29,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
 
-ALLOWED_HOSTS = ['8000-dylankane-hardcopy-p6phh15kb9b.ws-eu105.gitpod.io']
+ALLOWED_HOSTS = [
+    '8000-dylankane-hardcopy-p6phh15kb9b.ws-eu105.gitpod.io',
+    'https://hard-copy-dca8835ff546.herokuapp.com/'
+    ]
 
 
 # Application definition
@@ -122,12 +125,17 @@ LOGIN_REDIRECT_URL = '/'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+    else:
+        DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': BASE_DIR / 'db.sqlite3',
+            }
+        }
 
 
 # Password validation
