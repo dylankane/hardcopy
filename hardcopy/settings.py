89f34ccf21c7f849c7fee01c5e78f2cb31b7d3f4
Dylan <dylankane3@gmail.com>
2023-10-24 22:21:54 +0000

    Fix syntax error in settings.py

--- hardcopy/settings.py
+++ hardcopy/settings.py
@@ -129,13 +129,13 @@ if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-    else:
-        DATABASES = {
-            'default': {
-                'ENGINE': 'django.db.backends.sqlite3',
-                'NAME': BASE_DIR / 'db.sqlite3',
-            }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
         }
+    }
 
 
 # Password validation
