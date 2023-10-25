9f056ca4948d43d537af1a15da10cc4f68a85d4b
Dylan <dylankane3@gmail.com>
2023-10-25 00:18:15 +0000

    Fix syntax error in settings.py

--- hardcopy/settings.py
+++ hardcopy/settings.py
@@ -185,8 +185,8 @@ if 'USE_AWS' in os.environ:
     # bucket configuration
     AWS_STORAGE_BUCKET_NAME = 'hard-copy'
     AWS_S3_REGION_NAME = 'eu-west-1'
-    AWS_ACCESS_KEY_ID = os.environ.get(AWS_ACCESS_KEY_ID)
-    AWS_SECRET_ACCESS_KEY = os.environ.get(AWS_SECRET_ACCESS_KEY)
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
