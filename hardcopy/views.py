3c8ecb23dc71874fa2fd788a9d7f890588687d25
Dylan <dylankane3@gmail.com>
2023-10-26 23:39:15 +0000

    Add error hanling functions and templates

--- hardcopy/views.py
+++ hardcopy/views.py
@@ -4,3 +4,18 @@ from django.shortcuts import render
 def handler404(request, exception):
     """ Error Handler 404 - Page Not Found """
     return render(request, "errors/404.html", status=404)
+
+
+def handler403(request, exception):
+    """ Error Handler 403 - Page Not Found """
+    return render(request, "errors/403.html", status=404)
+
+
+def handler401(request, exception):
+    """ Error Handler 401 - Page Not Found """
+    return render(request, "errors/401.html", status=404)
+
+
+def handler500(request, exception):
+    """ Error Handler 500 - Page Not Found """
+    return render(request, "errors/500.html", status=404)
